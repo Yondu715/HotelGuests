dbf5181d6787155517b6f15097155b2e511aad3d
Yondu715 <maxv153218@gmail.com>
2024-07-05 15:29:04 +0700

    feat: add endpoints

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\GuestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::patterns([
+    'guestId' => '[0-9]+'
+]);
+
+Route::prefix('guests')->group(function () {
+    Route::get('/', [GuestController::class, 'index']);
+    Route::post('/', [GuestController::class, 'store']);
+    Route::get('/{guestId}', [GuestController::class, 'show']);
+    Route::patch('/{guestId}', [GuestController::class, 'update']);
+    Route::delete('/{guestId}', [GuestController::class, 'destroy']);
+});
+
